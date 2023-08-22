a5c304710a5e5791cb5dfc91c1751b8b8eff884b
dinanli <fyzx_ldn@outlook.com>
2023-08-22 21:01:48 +0200

    8/21 Ch.9

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -10,6 +10,18 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
     }
 
     @Override
+    public Object visitLogicalExpr(Expr.Logical expr){
+        Object left = evaluate(expr.left);
+
+        if(expr.operator.type == TokenType.OR){
+            if (isTruthy(left)) return left;
+        } else{
+            if (!isTruthy(left)) return left;
+        }
+        return evaluate(expr.right);
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr){
         return evaluate(expr.expression);
     }
@@ -72,6 +84,25 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
         stmt.accept(this);
     }
 
+    void executeBlock(List<Stmt> statements, Environment environment){
+        Environment previous = this.environment;
+        try{
+            this.environment = environment;
+
+            for (Stmt statement : statements){
+                execute(statement);
+            }
+        }finally {
+            this.environment = previous;
+        }
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt){
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
     @Override
     public Object visitBinaryExpr(Expr.Binary expr){
         Object left = evaluate(expr.left);
@@ -124,6 +155,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
     }
 
     @Override
+    public Void visitIfStmt(Stmt.If stmt){
+        if (isTruthy(evaluate(stmt.condition))){
+            execute(stmt.thenBranch);
+        } else if(stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
+        return null;
+    }
+
+    @Override
     public Void visitPrintStmt(Stmt.Print stmt){
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
@@ -139,6 +180,21 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
 
         environment.define(stmt.name.lexeme, value);
         return null;
+    }
+
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt){
+        while(isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr){
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
     }
 
     @Override
--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -13,17 +13,23 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         defineAst(outputDir, "Expr", Arrays.asList(
+                "Assign : Token name, Expr value",
                 "Binary: Expr left, Token operator, Expr right",
                 "Grouping: Expr expression",
                 "Literal: Object value",
+                "Logical : Expr left, Token operator, Expr right",
                 "Variable: Token name",
                 "Unary: Token operator, Expr right"
         ));
 
         defineAst(outputDir, "Stmt", Arrays.asList(
+                "Block : List<Stmt> statements",
                 "Expression: Expr expression",
+                "If : Expr condition, Stmt thenBranch," +
+                "Stmt elseBranch",
                 "Var: Token name, Expr initializer",
-                "Print: Expr expression"
+                "Print: Expr expression",
+                "While : Expr condition, Stmt body"
         ));
     }
 
